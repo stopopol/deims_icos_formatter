af47bf352703a55af475bbab218db5b8742a199c
stopopol <christoph.wohner@umweltbundesamt.at>
2025-06-16 13:25:05 +0200

    Update DeimsIcosFormatter.php

--- src/Plugin/Field/FieldFormatter/DeimsIcosFormatter.php
+++ src/Plugin/Field/FieldFormatter/DeimsIcosFormatter.php
@@ -99,13 +99,13 @@ class DeimsIcosFormatter extends FormatterBase {
 			
 			$output = "";
 		  
-			$api_url = "https://meta.icos-cp.eu/sparqlclient/";
+			$api_url = "https://meta.icos-cp.eu/sparqlclient/?type=JSON";
 			
 			
 			try {
 				$response = \Drupal::httpClient()->get($api_url, array(
 					'query' => $params,
-					'headers' => array('Accept' => 'text/html'))
+					'headers' => array('Accept' => 'application/json'))
 				);
 				$data = (string) $response->getBody();
 				if (empty($data)) {
