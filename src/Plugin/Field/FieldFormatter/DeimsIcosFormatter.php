2e9d82a4f8e5e1164cb1483619989add736b2aad
stopopol <christoph.wohner@umweltbundesamt.at>
2025-06-18 16:19:40 +0200

    Update DeimsIcosFormatter.php

--- src/Plugin/Field/FieldFormatter/DeimsIcosFormatter.php
+++ src/Plugin/Field/FieldFormatter/DeimsIcosFormatter.php
@@ -125,8 +125,8 @@ class DeimsIcosFormatter extends FormatterBase {
 					}
 					
 					$dataset_list .= "</ul>";
-					$output = "There is data for this site in the ICOS Data Portal. The latest datasets are:<br>" .$dataset_list;
-					$output .= "<br>Click here to <a href='$landing_page_url'>visit the ICOS Data Portal.</a>";
+					$output = "There is data for this site in the ICOS Data Portal. The latest datasets are:<br>" . $dataset_list;
+					$output .= "Click here to <a href='$landing_page_url'>visit the ICOS Data Portal.</a>";
                 }
 			}
 			catch (GuzzleException $e) {
